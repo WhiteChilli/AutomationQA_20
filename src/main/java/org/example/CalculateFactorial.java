5e7aeccc6a5ce09fc888512109bd4bf21370e926
WhiteChilli <locmerea@gmail.com>
2023-05-01 13:53:41 +0300

    added CalculateFactorial class and method

---
+++ src/main/java/org/example/CalculateFactorial.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.Scanner;
+
+public class CalculateFactorial {
+
+    public static int factorial() {
+        Scanner userInput = new Scanner(System.in);
+        int numberEntered = 0;
+
+        try {
+            System.out.print("Calculate factorial number of: ");
+            numberEntered = userInput.nextInt();
+        } catch (NullPointerException e) {
+            System.out.println("Error:" + e.getMessage());
+            userInput.nextLine();
+        } catch (ArithmeticException e) {
+            System.out.println("Error:" + e.getMessage());
+            userInput.nextLine();
+        } catch (NumberFormatException e) {
+            System.out.println("Error:" + e.getMessage());
+            userInput.nextLine();
+
+        } finally {
+            userInput.close();
+        }
+
+        int factorialResult = 1;
+        for (int i = 1; i <= numberEntered; i++) {
+            factorialResult *= i;
+        }
+        return factorialResult;
+    }
+}
+
